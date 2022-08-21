f37a0c8ec7ae9782324441323913f259e4f0f6f6
Léderer András <andrew@file.glass>
2022-08-22 00:06:55 +0200

    try 2

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const core = require('@actions/core');
 const fs = require("fs")
 
-
-const parser = require('./parser');
-const compiler = require('./compiler');
-
-const parse = (input) => compiler.compile(parser.parse(input.toString()))
+const toml = require("toml")
 
 try {
     const path = core.getInput("path")
     const tomlFile = fs.readFileSync(path).toString()
-    const {package} = parse(tomlFile)
+    const {package} = toml.parse(tomlFile)
     core.setOutput("version", package.version)
     core.setOutput("crateName", package.name)
     core.setOutput("lastBuiltCrateName", `${package.name}-${package.version}.crate`)
